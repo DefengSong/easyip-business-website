ac71de785b7e84474ab36bab4abbd705da4cffa8
DefengSong <yuelai.song@gmail.com>
2026-04-24 21:49:09 +0800

    Update route.ts

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -17,7 +17,7 @@ export async function POST(request: Request) {
 
     // Send email using Resend
     const { data, error } = await resend.emails.send({
-      from: "EasyIP Contact Form <onboarding@resend.dev>",
+      from: from: "EasyIP Contact Form <info@easyipgroup.com>",
       to: "songdefeng@yahoo.com",
       subject: `New Contact Form Submission from ${name}`,
       html: `
